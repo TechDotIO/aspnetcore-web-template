8614826d0aa978917e287e0cef04faae3f64a97b
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-07-20 21:25:25 -0700

    Initial commit

---
+++ ASPNETCoreTemplate/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Tech.io ASP .NET Core Web Template";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "community@tech.io";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
